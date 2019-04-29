2501cfcc8bb588d7c0f17c18457822f7d7d0f1bd
keyflight <olivier@andev.nl>
2019-04-29 18:12:19 +0700

    Small improvements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {css} from 'emotion'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
+export const Canvas = ({children}) => (
+    <div
+        className={css`
+            padding: 30px 15px;
+            background-color: #fafafa;
+            @media (min-width: 768px) {
+                padding: 50px 100px;
+            }
+        `}
+    >
+        {children}
     </div>
-  }
-}
+)
+
+export const Box = ({children}) => (
+    <div
+        className={css`
+            background: #fff;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 1px 2px 0px;
+    border-radius: 5px;
+    margin-bottom: 50px;
+    padding: 30px;
+    -webkit-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+    &:hover {
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 2px 2px 0px;
+    }
+        `}
+    >
+        {children}
+    </div>
+)
+
+export const Paragraph = ({children}) => (
+    <div
+        className={css`
+color: #999;
+font-size: 12px;
+margin: 0 0 20px 0;
+        `}
+    >
+        {children}
+    </div>
+)
+
+export const Heading = ({children}) => (
+    <h2
+        className={css`
+font-weight: 400;
+font-size: 24px;
+        `}
+    >
+        {children}
+    </h2>
+)
